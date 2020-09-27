acb9b048511dc95db54dcc0f0fb2f5c0aea4582d
Noyan Alimov <noyan.alimov@gmail.com>
2020-09-27 15:48:37 +0100

    added successful post request

--- mongodb.utils.js
+++ mongodb.utils.js
@@ -5,14 +5,7 @@ dotenv.config();
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_URL}/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const connectToDB = () => {
-    client.connect(err => {
-        try {
-            console.log('DB connected')
-        } catch (error) {
-            console.log(error)
-        }
-    });
-}
 
-module.exports = connectToDB;
+module.exports = {
+    client
+};
--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 
-const connectToDB = require('./mongodb.utils');
+const client = require('./mongodb.utils').client;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-connectToDB();
+app.post('/add-movie', async (req, res) => {
+    try {
+        const movie = req.body;
 
+        await client.connect();
+        const db = client.db('MoviesClub');
+        const collection = db.collection('Movies');
+        await collection.insertOne(movie);
 
+        res.status(201).json({
+            success: true,
+            data: movie
+        });
+        
+        await client.close();
+    } catch (error) {
+        console.log('error with post request add movie: ', error);
+        res.status(500).json({
+            success: false
+        });
+    }
+})
 
 const port = 8000
 app.listen(port, () => {
